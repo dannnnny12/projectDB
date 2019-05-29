2b9f13a2e0ffe0163fbd5cb0e20e44ab3aa92969
Albert Liu <v36239@gmail.com>
2019-05-29 14:20:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
@@ -60,5 +61,4 @@ class Ui_Dialog(object):
         self.viewOurCompanies.setText(_translate("Dialog", "View Our Companies"))
         self.pushButton.setText(_translate("Dialog", "Purchasing History"))
         self.placeAnOrder.setText(_translate("Dialog", "Place an Order"))
-
 
